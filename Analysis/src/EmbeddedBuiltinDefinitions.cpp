ef6061301660ff0ba9ac3b44db8ab86510b07f27
PhoenixWhitefire <86601049+PhoenixWhitefire@users.noreply.github.com>
2026-09-09 21:08:32 +0530

    move metatable functors to `table` library definitions section

--- Analysis/src/EmbeddedBuiltinDefinitions.cpp
+++ Analysis/src/EmbeddedBuiltinDefinitions.cpp
@@ -61,62 +61,6 @@ declare function loadstring<A...>(src: string, chunkname: string?): (((A...) -> 
 -- Cannot use `typeof` here because it will produce a polytype when we expect a monotype.
 declare function unpack<V>(tab: {V}, i: number?, j: number?): ...V
 
-declare function getmetatable<T>(tab: T): getmetatable<T>
-declare function setmetatable<T, MT>(tab: T, meta: MT): setmetatable<T, MT>
-
-)BUILTIN_SRC";
-
-static constexpr const char* kBuiltinDefinitionBaseSrc_OldSolver = R"BUILTIN_SRC(
-
-@checked declare function require(target: any): any
-
-@checked declare function getfenv(target: any): { [string]: any }
-
-declare _G: any
-declare _VERSION: string
-
-declare function gcinfo(): number
-
-declare function print<T...>(...: T...)
-
-declare function type<T>(value: T): string
-declare function typeof<T>(value: T): string
-
--- `assert` has a magic function attached that will give more detailed type information
-declare function assert<T>(value: T, errorMessage: string?): T
-declare function error<T>(message: T, level: number?): never
-
-declare function tostring<T>(value: T): string
-declare function tonumber<T>(value: T, radix: number?): number?
-
-declare function rawequal<T1, T2>(a: T1, b: T2): boolean
-declare function rawget<K, V>(tab: {[K]: V}, k: K): V?
-declare function rawset<K, V>(tab: {[K]: V}, k: K, v: V): {[K]: V}
-declare function rawlen<K, V>(obj: {[K]: V} | string): number
-
-declare function setfenv<T..., R...>(target: number | (T...) -> R..., env: {[string]: any}): ((T...) -> R...)?
-
-declare function ipairs<V>(tab: {V}): (({V}, number) -> (number?, V), {V}, number)
-
-declare function pcall<A..., R...>(f: (A...) -> R..., ...: A...): (boolean, R...)
-
--- FIXME: The actual type of `xpcall` is:
--- <E, A..., R1..., R2...>(f: (A...) -> R1..., err: (E) -> R2..., A...) -> (true, R1...) | (false, R2...)
--- Since we can't represent the return value, we use (boolean, R1...).
-declare function xpcall<E, A..., R1..., R2...>(f: (A...) -> R1..., err: (E) -> R2..., ...: A...): (boolean, R1...)
-
--- `select` has a magic function attached to provide more detailed type information
-declare function select<A...>(i: string | number, ...: A...): ...any
-
--- FIXME: This type is not entirely correct - `loadstring` returns a function or
--- (nil, string).
-declare function loadstring<A...>(src: string, chunkname: string?): (((A...) -> any)?, string?)
-
-@checked declare function newproxy(mt: boolean?): any
-
--- Cannot use `typeof` here because it will produce a polytype when we expect a monotype.
-declare function unpack<V>(tab: {V}, i: number?, j: number?): ...V
-
 )BUILTIN_SRC";
 
 static constexpr const char* kBuiltinDefinitionBit32Src = R"BUILTIN_SRC(
@@ -250,6 +194,35 @@ declare coroutine: {
 )BUILTIN_SRC";
 
 static constexpr const char* kBuiltinDefinitionTableSrc = R"BUILTIN_SRC(
+
+declare function getmetatable<T>(tab: T): getmetatable<T>
+declare function setmetatable<T, MT>(tab: T, meta: MT): setmetatable<T, MT>
+
+declare table: {
+    concat: <V>(t: {V}, sep: string?, i: number?, j: number?) -> string,
+    insert: (<V>(t: {V}, value: V) -> ()) & (<V>(t: {V}, pos: number, value: V) -> ()),
+    maxn: <V>(t: {V}) -> number,
+    remove: <V>(t: {V}, number?) -> V?,
+    sort: <V>(t: {V}, comp: ((V, V) -> boolean)?) -> (),
+    create: <V>(count: number, value: V?) -> {V},
+    find: <V>(haystack: {V}, needle: V, init: number?) -> number?,
+
+    unpack: <V>(list: {V}, i: number?, j: number?) -> ...V,
+    pack: <V>(...V) -> { n: number, [number]: V },
+
+    getn: <V>(t: {V}) -> number,
+    foreach: <K, V>(t: {[K]: V}, f: (K, V) -> ()) -> (),
+    foreachi: <V>({V}, (number, V) -> ()) -> (),
+
+    move: <V>(src: {V}, a: number, b: number, t: number, dst: {V}?) -> {V},
+
+    clear: (table: {}) -> (),
+    isfrozen: (t: {}) -> boolean,
+}
+
+)BUILTIN_SRC";
+
+static constexpr const char* kBuiltinDefinitionTableSrc_OldSolver = R"BUILTIN_SRC(
 
 declare table: {
     concat: <V>(t: {V}, sep: string?, i: number?, j: number?) -> string,
@@ -453,18 +426,13 @@ declare class: {
 
 std::string getBuiltinDefinitionSource(SolverMode Solver)
 {
-    std::string result;
-
-    if (Solver == SolverMode::New && FFlag::LuauNewSolverNewDefinitions)
-        result = kBuiltinDefinitionBaseSrc;
-    else
-        result = kBuiltinDefinitionBaseSrc_OldSolver;
+    std::string result = kBuiltinDefinitionBaseSrc;
 
     result += kBuiltinDefinitionBit32Src;
     result += kBuiltinDefinitionMathSrc;
     result += kBuiltinDefinitionOsSrc;
     result += kBuiltinDefinitionCoroutineSrc;
-    result += kBuiltinDefinitionTableSrc;
+    result += Solver == SolverMode::New && FFlag::LuauNewSolverNewDefinitions ? kBuiltinDefinitionTableSrc : kBuiltinDefinitionTableSrc_OldSolver;
     result += kBuiltinDefinitionDebugSrc;
     result += kBuiltinDefinitionUtf8Src;
     if (FFlag::LuauIntegerType2 && FFlag::LuauIntegerLibrary)
